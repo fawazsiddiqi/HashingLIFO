0fcbd067d5e82cbd4d3b671bfa430852ae363882
Fawaz <fawazsiddiqi@yahoo.com>
2018-05-14 16:48:34 +0400

    Initial Commit

---
+++ HashingLIFO/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashingLIFO
+//
+//  Created by Fawaz Siddiqi on 5/14/18.
+//  Copyright © 2018 Fawaz Siddiqi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
